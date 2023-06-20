167e591b8a27872ba0dd7a3b7fee960dfb12e4c8
Mitch Rogers <mitchellmel@outlook.com>
2023-06-19 20:10:55 -0500

    init commit

---
+++ spot/spotApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotApp.swift
+//  spot
+//
+//  Created by Mitch Rogers on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct spotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
